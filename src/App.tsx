723ecbe09dc830a269c4b2cfa8c7e7a3d9fc155e
Carlos H Sánchez <carloshiramsanchez@gmail.com>
2025-03-27 17:32:32 -0700

    loading spinnaaa

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,61 @@ import {
   AppWindow,
 } from "lucide-react";
 import Timeline from "./components/Timeline/Timeline";
-import { JavaScript, Nodejs, CSS, Expressjs, HTML5, MongoDB } from "./components/icons/icons";
+import {
+  JavaScript,
+  Nodejs,
+  CSS,
+  Expressjs,
+  HTML5,
+  MongoDB,
+} from "./components/icons/icons";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const handleLoad = () => {
+      setTimeout(() => setIsLoading(false), 500);
+    };
+
+    if (document.readyState === "complete") {
+      handleLoad();
+    } else {
+      window.addEventListener("load", handleLoad);
+    }
+
+    return () => {
+      window.removeEventListener("load", handleLoad);
+    };
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="h-screen w-screen flex items-center justify-center bg-gray-900">
+        <div role="status">
+          <svg
+            aria-hidden="true"
+            className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300"
+            viewBox="0 0 100 101"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              fill="currentColor"
+            />
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentFill"
+            />
+          </svg>
+          <span className="sr-only">Loading...</span>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="min-h-screen bg-gray-900 relative overflow-hidden">
       {/* Background particles */}
@@ -35,8 +87,8 @@ function App() {
         <div className="bg-gradient-to-r from-gray-800 to-gray-900 p-6 rounded-lg border border-gray-700 shadow-lg mt-8 w-full md:w-2/4 flex flex-col md:flex-row">
           {/* My cute face */}
           <img
-            src="https://github.com/hiiramsan.png"
-            className="rounded-lg mb-4 md:mb-0 md:mr-4 w-32 h-32 md:w-48 md:h-48"
+            src="me.jpg"
+            className="rounded-lg mb-4 md:mb-0 md:mr-4 w-32 h-32 md:w-48 md:h-48 border border-gray-700"
           ></img>
           {/* Info at the right */}
           <div>
@@ -120,23 +172,23 @@ function App() {
               target="_blank"
             >
               <img
-              src="/flashy.png"
-              alt="Flashy"
-              className="w-full h-[180px] object-cover rounded-t-lg"
+                src="/flashy.png"
+                alt="Flashy"
+                className="w-full h-[180px] object-cover rounded-t-lg"
               />
               <p className="text-md text-white ml-4 mt-4 font-semibold">
-              Flashy
+                Flashy
               </p>
               <p className="text-md text-gray-300 ml-4 mb-2 font-light">
-              A simple flashcard app
+                A simple flashcard app
               </p>
               <div className="flex justify-start items-center ml-4 mb-4 space-x-2">
-              <Nodejs className="size-6"/>
-              <JavaScript className="size-6" />
-              <CSS className="size-6" />
-              <Expressjs className="size-6" />
-              <HTML5 className="size-6" />
-              <MongoDB className="size-6" />
+                <Nodejs className="size-6" />
+                <JavaScript className="size-6" />
+                <CSS className="size-6" />
+                <Expressjs className="size-6" />
+                <HTML5 className="size-6" />
+                <MongoDB className="size-6" />
               </div>
             </a>
 
